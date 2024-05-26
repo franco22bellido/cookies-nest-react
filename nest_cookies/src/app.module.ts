4ad50a7149a2ff78566d8303d47f3224e4e2d7ca
franco22bellido <franco.bellido22@gmail.com>
2024-05-26 05:16:22 -0300

    redirects file react netlify

--- nest_cookies/src/app.module.ts
+++ nest_cookies/src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(cookieParser()).forRoutes('*');
+  }
+}
